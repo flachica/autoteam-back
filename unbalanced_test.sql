78faf29f89bcefed02a50214cda01779bd858a34
Fernando La Chica <fernandolachica@gmail.com>
2024-12-29 11:13:52 +0100

    SQL tunned

--- unbalanced_test.sql
+++ unbalanced_test.sql
@@ -4,25 +4,25 @@ from
 Select d.id, d.fullname, round(max(d.balance), 2) balance, round(sum(coalesce(d.price, 0)), 2) calculated_balance, count(d.courtId) court_count
 from
 (
-    SELECT p.id, p.name || coalesce(' ' || p.surname,'') fullname, p.balance, c.id courtId, -1*c.price price, c.date, "OWN" court_type, c.state
+    SELECT p.id, p.name || coalesce(' ' || p.surname,'') fullname, p.balance, c.id courtId, c.date courtDate, c.state courtState, -1*c.price price, c.date, "OWN" court_type, c.state
     FROM player p 
     left join court_players_player cpp on p.id=cpp.playerId
     left join court c on cpp.courtId=c.id
-    where coalesce(c.state,'') in ('reserved', 'expired', '')
+    where c.reservationId is not null and coalesce(c.state,'') in ('reserved', 'expired', '')
     union all
-    SELECT p.id, p.name || coalesce(' ' || p.surname,'') fullname, p.balance, c.id courtId, -1*c.price price, c.date, "KNOWN" court_type, c.state
+    SELECT p.id, p.name || coalesce(' ' || p.surname,'') fullname, p.balance, c.id courtId, c.date courtDate, c.state courtState, -1*c.price price, c.date, "KNOWN" court_type, c.state
     FROM player p 
     inner join invited_player ip on p.id=ip.payerPlayerId
     left join court c on ip.courtId=c.id
-    where coalesce(c.state,'') in ('reserved', 'expired', '')
+    where c.reservationId is not null and  coalesce(c.state,'') in ('reserved', 'expired', '')
     union all
-    SELECT p.id, p.name || coalesce(' ' || p.surname,'') fullname, p.balance, c.id courtId, -1*c.price price, c.date, "UNKNOWN" court_type, c.state
+    SELECT p.id, p.name || coalesce(' ' || p.surname,'') fullname, p.balance, c.id courtId, c.date courtDate, c.state courtState, -1*c.price price, c.date, "UNKNOWN" court_type, c.state
     FROM player p 
     inner join invited_anon_player iap on p.id=iap.payerPlayerId
     left join court c on iap.courtId=c.id
-    where coalesce(c.state,'') in ('reserved', 'expired', '')
+    where c.reservationId is not null and  coalesce(c.state,'') in ('reserved', 'expired', '')
     union all
-    Select p.id, p.name || coalesce(' ' || p.surname,'') fullname, p.balance, null courtId, m.amount price, m.date, 'MOVEMENT' court_type, null state
+    Select p.id, p.name || coalesce(' ' || p.surname,'') fullname, p.balance, null courtId, null courtDate, null courtState, m.amount price, m.date, 'MOVEMENT' court_type, null state
     from movement m
     inner join player p on m.playerId=p.id
     where (m.type = 'in' or m.courtId is null ) and m.validated=1
